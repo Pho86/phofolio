cccda248981559fdb9105d3b16905991108ff84a
pho86@my.bcit.ca <pho86@my.bcit.ca>
2023-01-22 21:31:57 -0800

    fix responsiveness of project cards being flipped

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -18,58 +18,11 @@ export const Project = component$((props: projectProps) => {
    return (
       <div class={twMerge(
          props.fadeIn ? "animate-fadeInLeft delay-1000" : "animate-fadeInRight delay-1000",
-         "w-full bg-gradient-to-l from-stone-700 to-neutral-800 rounded-2xl px-6 py-6 grid grid-cols-1 gap-6 md:grid-cols-2"
+         "w-full bg-gradient-to-l  from-stone-700 to-neutral-800 rounded-2xl px-6 py-6 grid gap-6 "
       )}>
          {props.align ? <>
-            <div class="flex justify-center w-full">
-               {props.doubleimage &&
-                  <div class="flex justify-between w-full gap-2">
-                     <img src={props.doubleimage[0]} class="w-1/2 justify-self-center" />
-                     <img src={props.doubleimage[1]} class="w-1/2 justify-self-center" />
-                  </div>
-               }
-               {props.image && <img src={props.image} class="rounded justify-self-center md:w-full" />}
-            </div>
-            <div class="flex flex-col justify-between gap-3">
-               <div class="flex justify-between">
-                  <a href={props.url} title={props.title}>
-                     <h2 class="text-2xl">{props.title}</h2>
-                  </a>
-                  <div class="flex gap-2">
-                     <a href={props.giturl} title={"See Github"}>
-                        <img src="/github.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
-                     </a>
-                     <a href={props.liveurl} title={"See Live Site"}>
-                        <img src="/external.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
-                     </a>
-                  </div>
-               </div>
-               <p>{props.text}</p>
-               <p><b>Tech used:</b> {props.tools}</p>
-               <Button text="Learn More" url={props.url} />
-            </div>
-         </> :
-            <>
+            <div class="flex flex-col gap-6 md:grid md:grid-cols-2">
                <div class="flex justify-center w-full">
-                  <div class="flex flex-col justify-between gap-3">
-                     <div class="flex justify-between">
-                        <a href={props.url} title={props.title}>
-                           <h2 class="text-2xl">{props.title}</h2>
-                        </a>
-                        <div class="flex gap-2">
-                           <a href={props.giturl} title={"See Github"}>
-                              <img src="/github.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
-                           </a>
-                           <a href={props.liveurl} title={"See Live Site"}>
-                              <img src="/external.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
-                           </a>
-                        </div>
-                     </div>
-                     <p>{props.text}</p>
-                     <p><b>Tech used:</b> {props.tools}</p>
-                     <Button text="Learn More" url={props.url} />
-                  </div>
-               </div>
                   {props.doubleimage &&
                      <div class="flex justify-between w-full gap-2">
                         <img src={props.doubleimage[0]} class="w-1/2 justify-self-center" />
@@ -77,6 +30,57 @@ export const Project = component$((props: projectProps) => {
                      </div>
                   }
                   {props.image && <img src={props.image} class="rounded justify-self-center md:w-full" />}
+               </div>
+               <div class="flex flex-col justify-between gap-3">
+                  <div class="flex justify-between">
+                     <a href={props.url} title={props.title}>
+                        <h2 class="text-2xl">{props.title}</h2>
+                     </a>
+                     <div class="flex gap-2">
+                        <a href={props.giturl} title={"See Github"}>
+                           <img src="/github.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
+                        </a>
+                        <a href={props.liveurl} title={"See Live Site"}>
+                           <img src="/external.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
+                        </a>
+                     </div>
+                  </div>
+                  <p>{props.text}</p>
+                  <p><b>Tech used:</b> {props.tools}</p>
+                  <Button text="Learn More" url={props.url} />
+               </div>
+            </div>
+         </> :
+            <>
+               <div class="flex flex-col-reverse gap-6 md:grid md:grid-cols-2">
+                  <div class="flex justify-center w-full">
+                     <div class="flex flex-col justify-between gap-3">
+                        <div class="flex justify-between">
+                           <a href={props.url} title={props.title}>
+                              <h2 class="text-2xl">{props.title}</h2>
+                           </a>
+                           <div class="flex gap-2">
+                              <a href={props.giturl} title={"See Github"}>
+                                 <img src="/github.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
+                              </a>
+                              <a href={props.liveurl} title={"See Live Site"}>
+                                 <img src="/external.png" class="w-8 hover:scale-125 transition duration-75 hover:drop-shadow-lg" />
+                              </a>
+                           </div>
+                        </div>
+                        <p>{props.text}</p>
+                        <p><b>Tech used:</b> {props.tools}</p>
+                        <Button text="Learn More" url={props.url} />
+                     </div>
+                  </div>
+                  {props.doubleimage &&
+                     <div class="flex justify-between w-full gap-2">
+                        <img src={props.doubleimage[0]} class="w-1/2 justify-self-center" />
+                        <img src={props.doubleimage[1]} class="w-1/2 justify-self-center" />
+                     </div>
+                  }
+                  {props.image && <img src={props.image} class="rounded justify-self-center md:w-full" />}
+               </div>
             </>
          }
 
