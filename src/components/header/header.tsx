ea35d54d45474182f4137947c80bf081f60bce54
Philip Ho <pho86@my.bcit.ca>
2023-01-31 12:36:26 -0800

    github _blank and fix project alignment on homepage

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,18 +3,18 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <header class="bg-zinc-800 flex border-b-sky-600 border-b-8">
-      <div class="flex px-8 items-center font-bold text-2xl ">
-        <a class="flex justify-center" href="/" title="Go Back Home">
+      <a class="flex justify-center hover:bg-neutral-900" href="/" title="Go Back Home">
+        <div class="flex px-8 items-center font-bold text-2xl ">
           Home
-        </a>
-      </div>
+        </div>
+      </a>
       <ul class="m-0 pl-1 text-right flex-1 p-2">
-        <a href="https://github.com/Pho86" class="px-8 py-4 hover:font-bold hover:bg-neutral-900" >
+        <a href="https://github.com/Pho86" target="_blank" class="px-8 py-4 hover:bg-neutral-900" >
           <li class="inline-block h-full">
             Github
           </li>
         </a>
-        <a href="https://github.com/Pho86/" class="px-8 py-4 hover:font-bold hover:bg-neutral-900">
+        <a href="https://github.com/Pho86/" class="px-8 py-4 hover:bg-neutral-900">
           <li class="inline-block">
             See Resume
           </li>
