fa92be3e451a7aef263dcb699947c9f70c6b18cf
Philip Ho <pho86@my.bcit.ca>
2023-02-12 22:46:05 -0800

    fix import

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,6 +1,5 @@
 import { component$, Slot } from '@builder.io/qwik';
 import Header from '../components/header/header';
-import PDF from "/resume.pdf"
 
 export default component$(() => {
   return (
@@ -21,7 +20,7 @@ export default component$(() => {
         <a href="mailto:philip03ho@gmail.com" target="_blank">
           Email
         </a>
-        <a href={PDF} download={"Philip Ho Resume"}>
+        <a href={"/resume.pdf"} download={"Philip Ho Resume"}>
           Resume
         </a>
       </footer>
